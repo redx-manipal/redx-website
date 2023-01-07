748ddc1aa7b8543c8056d23665191fa826d79311
Prajna <prajnapprabhu3@gmail.com>
2023-01-07 08:13:21 +0530

    prod push

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,33 @@
+<<<<<<< HEAD
+=======
+import AboutUs from "../components/AboutUs";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Partners from "../components/Partners";
+import Team from "../components/Team";
+import Contact from '../components/Contact';
+import HeroSection from '../components/HeroSection';
+>>>>>>> f84ab03 (prod push)
 
 
 function Home() {
   return (
+<<<<<<< HEAD
     <div>Home</div>
+=======
+    <div>
+      <Navbar/>
+      <div className="mt-14">
+      <HeroSection/>
+      <AboutUs/>
+      <Team/>
+      <Partners/>
+      <Contact/>
+      <Footer/>
+      </div>
+   
+    </div>
+>>>>>>> f84ab03 (prod push)
   )
 }
 
